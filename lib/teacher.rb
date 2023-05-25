3e04bfd3fc18e8398fc3427eb8e2619bbc5b61e8
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-25 17:51:40 +0300

    update

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -5,11 +5,11 @@ class Teacher < User
 end
 
 describe Teacher do 
-  let!(:knowledge) { ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"] }
-  let!(:teacher) { Teacher.new }
+  let(:knowledge) { ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"] }
+  let(:teacher) { Teacher.new }
 
   it "inherits from the User class" do
-    expect(Teacher.superclass).to be(User) 
+    expect(Teacher.superclass).to eq(User) 
   end
   
   describe "#first_name" do 
