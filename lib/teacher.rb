a623ee80284832b3e7d2c9118e7d142c0656abaa
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-25 18:13:30 +0300

    update

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,34 +1,22 @@
-class Teacher < User
-  def teach(knowledge)
-    knowledge.sample
-  end
+class User
+  attr_accessor :first_name, :last_name
 end
 
-describe Teacher do 
-  let(:knowledge) { ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"] }
-  let(:teacher) { Teacher.new }
-
-  it "inherits from the User class" do
-    expect(Teacher.superclass).to eq(User) 
-  end
-  
-  describe "#first_name" do 
-    it "has a first name" do 
-      teacher.first_name = "Avi"
-      expect(teacher.first_name).to eq("Avi")
-    end
-  end
-
-  describe "#last_name" do 
-    it "has a last name" do 
-      teacher.last_name = "Flombaum"
-      expect(teacher.last_name).to eq("Flombaum")
-    end
+class Teacher < User
+  def initialize
+    @knowledge = [
+      "a String is a type of data in Ruby",
+      "programming is hard, but it's worth it",
+      "javascript async web request",
+      "Ruby method call definition",
+      "object oriented dog cat class instance",
+      "class method class variable instance method instance variable",
+      "programming computers hacking learning terminal",
+      "bash Ruby rvm update certs"
+    ]
   end
 
-  describe "#teach" do 
-    it "returns a random string of knowledge" do 
-      expect(knowledge).to include(teacher.teach(knowledge)) 
-    end
+  def teach
+    @knowledge.sample
   end
 end
